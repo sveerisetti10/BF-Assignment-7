6730cbe3fcbbf9540155b9019de26cce1948ead1
Sri Veerisetti <98369110+sveerisetti10@users.noreply.github.com>
2022-04-03 15:34:51 -0400

    Update app.R

--- app.R
+++ app.R
@@ -15,24 +15,27 @@ ui <- fluidPage(
     h6("To use this application, download the CSV deseq_res.csv from the data directory of this app's repository."),
     
     sidebarPanel(
-        fileInput("Browse", "Load differential expression results", accept = ".csv"), 
+        fileInput("input_data", "Load differential expression results", accept = ".csv"), 
 
         p("A volcano plot can be generated with log_2 fold-change on the x-axis and p-adjusted on the y-axis."),
         br(),
         
-        radioButtons("column_choice_x", "Choose the column for the x-axis", 
+        radioButtons("x_name", "Choose the column for the x-axis", 
                      c("baseMean", "log2FoldChange", "lfcSE", "stat", "pvalue", "padj")),
         
-        radioButtons("column_choice_y", "Choose the column for the y-axis", 
+        radioButtons("y_name", "Choose the column for the y-axis", 
                      c("baseMean", "log2FoldChange", "lfcSE", "stat", "pvalue", "padj")),
         
         colourInput("color_1", "Base Point Color", "C47DF0"), 
-        colourInput("colour_2", "Highlight Point Color", "#57E657"),
+        colourInput("color_2", "Highlight Point Color", "#57E657"),
         
-        sliderInput("p_adjusting", "Select the magnitude of the p adjusted coloring:", min = -300, max = 0, value = 30), 
+        sliderInput("slider", "Select the magnitude of the p adjusted coloring:", min = -300, max = 0, value = 30), 
         
-        submitButton("Plot", icon=NULL, width = 300)
-
+        submitButton("Plot", icon=NULL, width = 300),
+        
+        plotOutput(outputId = "volcano"),
+        tableOutput(outputId = "table")
+        
     )
 
 )
@@ -50,18 +53,20 @@ server <- function(input, output, session) {
     #' read.csv. Return this data frame in the normal return() style.
     load_data <- reactive({
         
+        read.csv(input$input_data$datapath, header = TRUE)
+        
         return()
     })
     
     #' Volcano plot
     #'
-    #' @param dataf The loaded data frame.
+    #' @param input_data The loaded data frame.
     #' @param x_name The column name to plot on the x-axis
     #' @param y_name The column name to plot on the y-axis
     #' @param slider A negative integer value representing the magnitude of
     #' p-adjusted values to color. Most of our data will be between -1 and -300.
-    #' @param color1 One of the colors for the points.
-    #' @param color2 The other colors for the points. Hexadecimal strings: "#CDC4B5"
+    #' @param color_1 One of the colors for the points.
+    #' @param color_2 The other colors for the points. Hexadecimal strings: "#CDC4B5"
     #'
     #' @return A ggplot object of a volcano plot
     #' @details I bet you're tired of these plots by now. Me too, don't worry.
@@ -73,15 +78,37 @@ server <- function(input, output, session) {
     #' !!sym() may be required to access column names in ggplot aes().
     #'
     #' @examples volcano_plot(df, "log2fc", "padj", -100, "blue", "taupe")
-    volcano_plot <-
-        function(dataf, x_name, y_name, slider, color1, color2) {
-
-            return()
-        }
-    
+    volcano_plot <-function(input_data, x_name, y_name, slider, color_1, color_2) {
+        
+        #The goal here is to develop another column named "sorting". In this column, 
+        #we will originally have "NA", but based on the input of the slider value, 
+        #the volcano plot should display whether it is above the user's threshold
+        #or not. 
+        input_data$sorting <- "NA"
+        input_data$sorting[input_data$padj < 1E(slider)] <- "TRUE"
+        input_data$sorting[input_data$padj > 1E(slider)] <- "FALSE"
+        
+        #The two color points are going to be based on what the user chooses
+        #These colors will then be assigned to either "TRUE" or "FALSE"
+        point_colors = c(color_1, color_2)
+        names(point_colors) <- c("TRUE", "FALSE")
+        
+        volcano_friend <- 
+            ggplot(input_data, aes(x = x_name, y = y_name, col = sorting)) +
+            geom_point() +
+            xlab(x_name)+
+            ylab(y_name)+
+            #The points of the colors will be based on the information above
+            scale_color_manual(values = point_colors) +
+            theme_bw()+
+            theme(legend.position = "bottom") +
+            theme(axis.text.x = element_text(angle = 90, hjust = 0, vjust = 0.5))
+        
+        return(volcano_friend)
+    }
     #' Draw and filter table
     #'
-    #' @param dataf Data frame loaded by load_data()
+    #' @param input_data Data frame loaded by load_data()
     #' @param slider Negative number, typically from the slider input.
     #'
     #' @return Data frame filtered to p-adjusted values that are less than 
@@ -98,18 +125,33 @@ server <- function(input, output, session) {
     #'    X  baseMean     log2FC     lfcSE      stat       pvalue         padj
     #'gene1 11690.780   9.852926 0.2644650  37.25607 8.45125e-304 1.54472e-299
     #'gene2  3550.435  -6.183714 0.1792708 -34.49369 9.97262e-261 9.11398e-257
-    draw_table <- function(dataf, slider) {
+    
+    draw_table <- function(input_data, slider) {
+        #This is done just to make sure that that input_data is in a tibble or
+        #dataframe format 
+        slider_tibble <- as.tibble(input_data)
+        format(slider_tibble)
         
+        #Here we are taking the tibble we just made and then we want to 
+        #specifically target out the padj values that are less than 1E(slider)
+        filtered_tible <- slider_tibble %>%
+            filter(padj < 1E(slider)) %>%
         return()
     }
     
     #' These outputs aren't really functions, so they don't get a full skeleton, 
     #' but use the renderPlot() and renderTabel() functions to return() a plot 
     #' or table object, and those will be displayed in your application.
-    output$volcano <- NULL # replace this NULL
+    output$volcano <- renderPlot({
+        return(volcano_friend)
+    
+    })
     
     # Same here, just return the table as you want to see it in the web page
-    output$table <- NULL # replace this NULL
+    output$table <- renderPlot({
+        return(filtered_table)
+        
+    })
 }
 
 # Run the application
